6050cb04084ebb9d0454664abe368837febfd334
James Blair <mail@jamesblair.net>
2023-12-04 14:33:14 +1300

    Fix links for next 13.

--- components/Tag.js
+++ components/Tag.js
@@ -3,12 +3,14 @@ import kebabCase from '@/lib/utils/kebabCase'
 
 const Tag = ({ text }) => {
   return (
-    <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-800 hover:text-primary-900 dark:hover:text-primary-400">
-        {text.split(' ').join('-')}
-      </a>
-    </Link>
-  )
+    (<Link
+      href={`/tags/${kebabCase(text)}`}
+      className="mr-3 text-sm font-medium uppercase text-primary-800 hover:text-primary-900 dark:hover:text-primary-400">
+
+      {text.split(' ').join('-')}
+
+    </Link>)
+  );
 }
 
 export default Tag
