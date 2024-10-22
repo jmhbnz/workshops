e8b416180efa84aa300c0e48e7c031e152373fc0
James Blair <mail@jamesblair.net>
2024-10-23 09:56:50 +1300

    Complete exercise 4.

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
     { href: '/workshop', title: 'Exercises' },
-    { href: 'https://docs.openshift.com/container-platform/4.15/welcome/index.html', title: 'Documentation' },
+    { href: 'https://docs.openshift.com/container-platform/4.16/welcome/index.html', title: 'Documentation' },
     { href: 'https://demo.redhat.com/workshop/s72ya3', title: 'Environment login' }
 ]
 
