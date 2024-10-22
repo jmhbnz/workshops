23b5ea24d8d5707d9d4e120208b0c7d5d82ad3cd
James Blair <mail@jamesblair.net>
2024-10-23 10:29:10 +1300

    Update environment links.

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
     { href: '/workshop', title: 'Exercises' },
     { href: 'https://docs.openshift.com/container-platform/4.16/welcome/index.html', title: 'Documentation' },
-    { href: 'https://demo.redhat.com/workshop/s72ya3', title: 'Environment login' }
+    { href: 'https://demo.redhat.com/workshop/cduffr', title: 'Environment login' }
 ]
 
 export default headerNavLinks
