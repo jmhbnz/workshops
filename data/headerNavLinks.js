e00f356705bb87a85f7dbe239da2c49a163bd05d
James Blair <mail@jamesblair.net>
2026-07-06 17:24:00 +1200

    Fix workshop typos.

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,6 @@
 const headerNavLinks = [
     { href: '/workshop', title: 'Exercises' },
     { href: 'https://docs.redhat.com/en/documentation/openshift_container_platform/4.20/html/overview/welcome-index.html', title: 'Documentation' },
-    { href: 'https://catalog.demo.redhat.com/workshop/djzt3z', title: 'Environment login' }
 ]
 
 export default headerNavLinks
